59d5a35df52ae2bb5bfa01b74220f9bb9c0e404a
Harsh3052 <56725880+Harsh3052@users.noreply.github.com>
2021-02-19 12:17:07 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-# print('hello from python')
-f=open("guru99.txt", "a+")
-for i in range(2):
-     f.write("Appended line %d\r\n" % (i+1))
+import os
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
+        
+
+# Example
+createFolder('./data/')
+# Creates a folder in the current directory called data
